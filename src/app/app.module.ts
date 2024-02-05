412f2f2841febe24591b61308fc03f947850e8fb
ozuro <alvaropacompialerma@gmail.com>
2024-02-04 19:55:51 -0500

    consumiendo api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/countries/page/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/page/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,9 +10,19 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
-  searchByCapital (term:string):void{
+  public countries: Country[] = [];
+
+  constructor(private countryService: CountryService) {
+
+  }
+
+  searchByCapital(term: string): void {
+
+    this.countryService.searchCapital(term).subscribe(countries => {
+      this.countries = countries;
+    });
     console.log('desde ByCapITALPage')
-    console.log({term});
+    console.log({ term });
   }
 
 
